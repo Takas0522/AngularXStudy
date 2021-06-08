39d51e441f342164799a3919ed79bef03259ca9e
Takas0522 <t_o_19880522@outlook.com>
2021-06-08 16:56:36 +0900

    version up

--- UnitTest/src/app/unit-test/unit-test.component.spec.ts
+++ UnitTest/src/app/unit-test/unit-test.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UnitTestComponent } from './unit-test.component';
 import { UnitTestService } from './unit-test.service';
@@ -8,7 +8,7 @@ describe('UnitTestComponent', () => {
   let fixture: ComponentFixture<UnitTestComponent>;
   let service: UnitTestService;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UnitTestComponent ],
       providers: [UnitTestService]
