d5e8bb5b6d4aa66ea76b855ce658c129d826e1f2
Takas0522 <t_o_19880522@outlook.com>
2021-07-14 06:44:00 +0900

    add

---
+++ learn-webpush/front/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'front';
+
+  constructor(
+    private swPush: SwPush
+  ) {}
+}
