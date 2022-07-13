8402e73cd15374ea8b2978da0b00c719d5dff72b
Takas0522 <t_o_19880522@outlook.com>
2022-07-14 00:47:46 +0900

    neru

--- AsyncValidatorToEnvInjector/src/app/app.component.ts
+++ AsyncValidatorToEnvInjector/src/app/app.component.ts
@@ -9,32 +9,39 @@ import { UserService } from './user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AsyncValidatorToEnvInjector';
-  envInjector = inject(EnvironmentInjector);
-
-
+  private envInjector = inject(EnvironmentInjector);
+  private fn = runInContext<AsyncValidatorFn>(this.envInjector, userExistsValidatorAsync);
   formGroup = new FormGroup({
-    userId: new FormControl('', [], [this.envInjector.runInContext])
-  })
+    userId: new FormControl('', null, this.fn)
+  });
+  onclick() {
+    const service = this.envInjector.runInContext(() => inject(UserService));
+    service.getUsers$().subscribe(x => console.log(x))
+  }
 }
 
-const TOKEN = new InjectionToken('TOKEN', {
-  providedIn: 'root',
-  factory: () => userExistsValidatorAsync,
-});
 
-const userExistsValidatorAsync = () => {
-  return (service: UserService) : AsyncValidatorFn  => {
-    return (ctrl: AbstractControl): Observable<ValidationErrors | null> => {
-      const userId = ctrl.value;
-      return service.getUsers$().pipe(
+function runInContext<T>(injector: EnvironmentInjector, fn: (inj: EnvironmentInjector) => T): T {
+  const token = new InjectionToken<T>('TOKEN');
+  const tmpInjector = createEnvironmentInjector([
+    {provide: token, useFactory: () => fn(injector)},
+  ], injector);
+
+  return tmpInjector.get(token);
+}
+
+const userExistsValidatorAsync = (injector: EnvironmentInjector): AsyncValidatorFn => {
+  return (ctrl: AbstractControl): Observable<ValidationErrors | null> => {
+    const userId = Number(ctrl.value);
+    const service = injector.runInContext(() => inject(UserService));
+    return service.getUsers$().pipe(
         map(m => {
-          if (m.map(item => item.id).includes(userId)) {
+          const ids = m.map(item => item.id);
+          if (ids.includes(userId)) {
             return null;
           }
           return { notExists: true };
         })
-      )
-    }
+    );
   }
-};
+}
